ebc198364e3c5f46977ba07e1488adcbfefbb69a
Tony.Vidao.iMac <yingmu52@msn.com>
2017-10-26 11:59:18 -0700

    define shape

---
+++ benchmark/shape.go
@@ -0,0 +1,24 @@
+package benchmark
+
+// Point represents a coordinates in Cartesian Coordinate System
+type Point struct {
+	x float64
+	y float64
+}
+
+// Square represents a container or a damaged area
+type Square struct {
+	Point
+	Width float64
+}
+
+// Circle represents a circle to be packed
+type Circle struct {
+	Point
+	Radius float64
+}
+
+// Container is a square at 0,0 with side equals to 1
+func Container() Square {
+	return Square{Point{0, 0}, 1}
+}
